9365381855c44f746d638f3225b8a51cb9385ae4
eladjmc <eladjmc88@gmail.com>
2023-01-19 21:31:11 +0200

    made some fixes to 3.1 task

--- 3/3.1.js
+++ 3/3.1.js
@@ -1,10 +1,12 @@
 const yearsToGrow = (p0, percent, aug, p) => {
-  if (p0 + p0 * (percent/100) + aug < p0) {
+// I know it might look like this is not needed but try to use the input: (1500000, 2.5, -100000, 2000000) with and without it and you will see
+  if (p0 + Math.floor(p0 * (percent/100)) + aug < p0) {
     return 0;
   }
+
   if (p0 >= p) {
     return 0;
   }
-  return 1 + yearsToGrow(p0 + p0 * (percent/100) + aug, percent,aug, p);
+  return 1 + yearsToGrow(p0 + Math.floor(p0 * (percent/100)) + aug, percent,aug, p);
 };
-console.log(yearsToGrow(1500000, 2.5, -10000, 2000000));
+console.log(yearsToGrow(1500000, 2.5, 10000, 2000000));

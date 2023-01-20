1d2c1bc82d2ea5d9e3317e6be36f254e57a6e82f
eladjmc <eladjmc88@gmail.com>
2023-01-20 12:31:14 +0200

    made changes to the 3.1 task

--- 3/3.1.js
+++ 3/3.1.js
@@ -1,12 +1,18 @@
 const yearsToGrow = (p0, percent, aug, p) => {
-// I know it might look like this is not needed but try to use the input: (1500000, 2.5, -100000, 2000000) with and without it and you will see
-  if (p0 + Math.floor(p0 * (percent/100)) + aug < p0) {
+  const growthInPercentage = Math.floor(p0 * (percent / 100));
+  const totalPopulationChanges = p0 +growthInPercentage + aug
+
+  // I know it might look like this is not needed but try to use the input: (1500000, 2.5, -100000, 2000000) with and without it and you will see
+  if (totalPopulationChanges < p0) {
     return 0;
   }
 
   if (p0 >= p) {
     return 0;
   }
-  return 1 + yearsToGrow(p0 + Math.floor(p0 * (percent/100)) + aug, percent,aug, p);
+  return (
+    1 +
+    yearsToGrow(totalPopulationChanges, percent, aug, p)
+  );
 };
 console.log(yearsToGrow(1500000, 2.5, 10000, 2000000));
